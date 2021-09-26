71f118d802de0e0e18c8d1c55a710e2795b35551
Whitneyto <nguyetto@udel.edu>
2021-09-26 16:05:00 -0400

    Showing a calculator

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { HeaderBar } from './components/HeaderBar';
 import { FooterBar } from './components/FooterBar';
+import { Calculator} from './components/Calculator';
 
 function App() {
   return (
     <div className="App">
       <HeaderBar></HeaderBar>
-      <p>haha</p>
+      <p className="big-title">Calculator</p>
+      <Calculator></Calculator>
       <FooterBar></FooterBar>
     </div>
   );
