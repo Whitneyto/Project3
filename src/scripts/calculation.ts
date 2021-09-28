fd939683f5a4debd2b8e00c22791e234a566b83b
Whitneyto <nguyetto@udel.edu>
2021-09-28 12:40:01 -0400

    Add scripts

--- src/scripts/calculation.ts
+++ src/scripts/calculation.ts
@@ -1,38 +1,11 @@
+export const re = /(?:(?:^|[-+_*/])(?:\s*-?\d+(\.\d+)?(?:[eE][+-]?\d+)?\s*))+$/;
 
-export function operationKey(operation: number[]) {
-  const opAdd = operation[0];
-  const opMinus = operation[1];
-  const opDiv = operation[2];
-  const opMul = operation[3];
-  if ((opAdd + opMinus + opDiv + opMul) === 1) {
-    return true;
-  } else {
-    return false;
+export function test(input:string) {
+  console.log("%s is valid? %s", input, re.test(input));
+  if (re.test(input)===true){
+    return eval(input).toString()
   }
-}
-
-export function assignNum(bool: boolean, input: string, num1: number) {
-  if (bool === true) {
-    return parseInt(input.replace(num1.toString(), ""));
-  } else {
-    return 0;
-  }
-}
-
-export function op(operation: number[], num1: number, num2: number) {
-  if ((operationKey(operation)) && (operation[0] === 1)) {
-    console.log(num1 +":"+num2);
-    return num1 + num2;
-  } else if ((operationKey(operation)) && (operation[1] === 1)) {
-    console.log(num1 +":"+num2);
-    return num1 - num2;
-  } else if ((operationKey(operation)) && (operation[2] === 1)) {
-    console.log(num1 +":"+num2);
-    return num1 * num2;
-  } else if ((operationKey(operation)) && (operation[3] === 1)) {
-    console.log(num1 +":"+num2);
-    return num1 / num2;
-  } else {
-    return 0;
+  else {
+    return "Invalid Input!"
   }
 }
